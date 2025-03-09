d79c11f7de942f3b076dc557b6da7a883b4af365
OllaWilk <alex.dev.wilk@gmail.com>
2025-03-09 13:44:00 +0100

    feat: randomize quiz questions order. - questions now appear in a random order to enhance the user experiance.

--- src/component/Quiz/Quiz.tsx
+++ src/component/Quiz/Quiz.tsx
@@ -5,6 +5,8 @@ import './Quiz.scss';
 export const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState<string[]>([]);
   const activeQuestionIndex = userAnswers.length;
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   const handleSelectedAnswer = (selectedAnswer: string) => {
     setUserAnswers((prevUserAnswer) => {
@@ -12,17 +14,19 @@ export const Quiz = () => {
     });
   };
   return (
-    <div id='question'>
-      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-      <ul id='answers'>
-        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
-          <li key={answer} className='answer'>
-            <button onClick={() => handleSelectedAnswer(answer)}>
-              {answer}
-            </button>
-          </li>
-        ))}
-      </ul>
+    <div id='quiz'>
+      <div id='question'>
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id='answers'>
+          {shuffledAnswers.map((answer) => (
+            <li key={answer} className='answer'>
+              <button onClick={() => handleSelectedAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
